e9decced8415bb1712cbecaeaca653215ac38868
Sunny Pacheco <pacheco.sunny@gmail.com>
2025-03-08 20:09:04 -0500

    update: update /fetchDealer routes

--- server/database/app.js
+++ server/database/app.js
@@ -58,17 +58,33 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
 
 // Express route to fetch all dealerships
 app.get('/fetchDealers', async (req, res) => {
-//Write your code here
+    try {
+        const documents = await Dealerships.find();
+        res.json(documents);
+    } catch (error) {
+        res.status(500).json({error: 'Error fetching dealership documents'});
+    }
 });
 
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
-//Write your code here
+    const state = req.params.state;
+    try {
+        const documents = await Dealerships.find({state});
+        res.json(documents);
+    } catch (error) {
+        res.status(500).json({error: 'Error fetching dealerships by state'})
+    }
 });
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
-//Write your code here
+    try {
+        const documents = await Dealerships.find({id: req.params.id});
+        res.json(documents);
+    } catch (error) {
+        res.status(500).json({error: 'Error fetching dealerships by id'});
+    }
 });
 
 //Express route to insert review
